d9dd451c70d2916f7c6bdae70ad896478d8dcbe5
abdulazeem <85398243+AzeemPasha471@users.noreply.github.com>
2021-11-11 02:54:22 +0530

    [pasha]Add: Windchill program added

---
+++ windchill.py
@@ -0,0 +1,12 @@
+try:
+    temperature = float(input("Enter temperature :"))
+    windSpeed = float(input("Enter windspeed  :"))
+
+    if temperature < 50 and windSpeed >= 3 and windSpeed < 120:
+        v = pow(windSpeed, 0.16)
+        windChill = 35.74 + 0.6215 * temperature + (0.4275 * temperature - 35.75) * v
+        print("Windchill  = {} ".format(windChill))
+
+except ValueError:
+    print("you can not enter string value !")
+
