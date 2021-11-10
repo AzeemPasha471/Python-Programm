b5215fc255a4332790cf73d3c50126325fa3119f
abdulazeem <85398243+AzeemPasha471@users.noreply.github.com>
2021-11-11 01:47:14 +0530

    Delete replace.py

--- replace.py
+++
@@ -1,9 +0,0 @@
-'''
-@Author: Md Azeem pasha
-@Date: 2021-11-07
-@Title :Replace String Template '''
-
-str1 = str(input("enter the username:\n"))
-txt= "Hello Username,How are you?"
-replaced= txt.replace("Username",str1,1)
-print(replaced)
