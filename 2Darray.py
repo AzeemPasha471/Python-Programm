22c149612afc1d4cd994df54258ab12b93065d06
abdulazeem <85398243+AzeemPasha471@users.noreply.github.com>
2021-11-13 00:51:39 +0530

    [Pasha]Add: added 2d array program

--- 2Darray.py
+++ 2Darray.py
@@ -5,11 +5,6 @@
 @Title :pritn the 2D array'''
 
 
-'''
-@Desc :  A library for reading in 2D arrays of integers, doubles, or booleans from
-standard input and printing them out to standard output.
-'''
-
 row_num = int(input("Input number of rows: "))
 col_num = int(input("Input number of columns: "))
 multi_list = [[0 for col in range(col_num)] for row in range(row_num)]
